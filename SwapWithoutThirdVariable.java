78cab62072f4ffd240cc8cf51e3d83b69dac1bdf
rahulpal <rahulpal1110@gmail.com>
2024-10-16 19:57:55 +0530

    swapping without using third variable by addition and substraction

---
+++ SwapWithoutThirdVariable.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SwapWithoutThirdVariable {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two values to be swapped :");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Value of a = " + a + " and value of b = " + b + " before swapping");
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("Value of a = " + a + " and value of b = " + b + " after swapping");
+
+    }
+    
+}
