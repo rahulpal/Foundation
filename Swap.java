880b7922fdcc8f7f7c7430f7f511519ea1c3fb12
rahulpal <rahulpal1110@gmail.com>
2024-10-16 19:38:58 +0530

    swaping two number using third variable

---
+++ Swap.java
@@ -0,0 +1,14 @@
+public class Swap{
+    public static void main(String[] args){
+        int a , b , c;
+        a = 2;
+        b = 3;
+        System.out.println("Value of a = " + a + " and b = " + b + " before swapping ");
+        c = a;
+        a = b;
+        b = c;
+        System.out.println("Value of a = " + a + " and b = " + b + " after swapping ");
+
+
+    }
+}
